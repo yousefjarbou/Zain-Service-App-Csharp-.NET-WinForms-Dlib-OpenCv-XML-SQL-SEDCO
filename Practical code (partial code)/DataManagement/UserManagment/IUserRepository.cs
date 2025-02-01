f53eaacfd9488ca3c8941aacae844f19c17ed726
Yousef Jarbou <166923297+yousefjarbou@users.noreply.github.com>
2025-02-01 15:09:30 +0300

    Add files via upload

---
+++ Practical code (partial code)/DataManagement/UserManagment/IUserRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace ZainCustomerService.DataManagement.UserManagment
+{
+    public interface IUserRepository
+    {
+        public void Add(User user);
+        public void Delete(string phoneNumber);
+        public List<string> LoadPhoneNumbers();
+        public User GetUserByPhoneNumber(string phoneNumber);
+        public List<User> LoadUsers(string phone="");
+    }
+}
---
+++ Practical code (partial code)/DataManagement/UserManagment/UserManager.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ZainCustomerService.DataManagement.PackageManagment;
+
+namespace ZainCustomerService.DataManagement.UserManagment
+{
+    public class UserManager
+    {
+        private readonly IUserRepository _userRepository;
+        private readonly PackageManager _packageManager;
+
+        public UserManager(IUserRepository userManager,PackageManager packageManager)
+        {
+            _userRepository = userManager;
+            _packageManager = packageManager;
+        }
+
+        public void CreateUser(string phoneNumber, string firstName, string lastName, double balance, string expiryDate, string packageName)
+        {
+            User newUser = new User(phoneNumber, firstName, lastName, balance, expiryDate, _packageManager.GetPackageByName(packageName));
+            _userRepository.Add(newUser);
+        }
+
+        public bool IsUserRegistered(string phoneNumber)
+        {
+            List<string> phonNumbers = _userRepository.LoadPhoneNumbers();
+            return phonNumbers.Contains(phoneNumber);
+        }
+
+        public User GetUserByPhoneNumber(string phoneNumber)
+        {
+            return _userRepository.GetUserByPhoneNumber(phoneNumber);
+        }
+
+
+        public List<User> LoadAllUsers()
+        {
+            return _userRepository.LoadUsers();
+        }
+
+        public void UpdateUser(User selectedUser)
+        {
+            _userRepository.Delete(selectedUser.PhoneNumber);
+            _userRepository.Add(selectedUser);
+        }
+
+        public List<User> SearchMatchedUserByPhoneNumber(string phone)
+        {
+            return _userRepository.LoadUsers(phone);
+        }
+
+        public void DeleteUser(string phoneNumber)
+        {
+            _userRepository.Delete(phoneNumber);
+        }
+
+        public static void CheckIfUserActive(ref string expiryDate, ref double balance, double monthlyPrice)
+        {
+            if (DateTime.TryParse(expiryDate, out DateTime parsedDate))
+            {
+                DateTime today = DateTime.Today;
+                if (parsedDate <= today && balance >= monthlyPrice)
+                {
+                    balance-=monthlyPrice;
+                    DateTime nextMonthDate = DateTime.Now.AddMonths(1);
+                    expiryDate= nextMonthDate.ToShortDateString();
+                }
+            }
+            else if (balance >= monthlyPrice)
+            {
+                balance -= monthlyPrice;
+                DateTime nextMonthDate = DateTime.Now.AddMonths(1);
+                expiryDate= nextMonthDate.ToShortDateString();
+            }
+        }
+    }
+}
+
+
