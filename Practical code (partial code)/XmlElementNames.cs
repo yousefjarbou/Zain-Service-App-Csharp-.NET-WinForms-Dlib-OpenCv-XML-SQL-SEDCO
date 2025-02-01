4a590147409c14a1c7267d63a6cc97f8c7a3bc73
Yousef Jarbou <166923297+yousefjarbou@users.noreply.github.com>
2025-02-01 15:11:37 +0300

    Add files via upload

---
+++ Practical code (partial code)/XmlElementNames.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZainCustomerService.Constants
+{
+    public static class XmlElementNames
+    {
+        public const string UserElement = "user";
+        public const string PhoneNumberElement = "phoneNumber";
+        public const string FirstNameElement = "firstName";
+        public const string LastNameElement = "lastName";
+        public const string PackageElement = "package";
+        public const string PriceElement = "price";
+        public const string IsPrepaidBoolElement = "isPrepaidBool";
+        public const string BalanceElement = "balance";
+        public const string ExpDateElement = "expDate";
+        public const string PrePaidElement = "prePaid";
+        public const string PostPaidElement = "postPaid";
+        public const string SimPriceElement = "simPrice";
+    }
+
+}
