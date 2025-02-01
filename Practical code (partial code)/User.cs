e955b41558e0f4906d544f72f15ead8de368c2e7
Yousef Jarbou <166923297+yousefjarbou@users.noreply.github.com>
2025-02-01 15:07:04 +0300

    Add files via upload

---
+++ Practical code (partial code)/User.cs
@@ -0,0 +1,32 @@
+﻿using ZainCustomerService.DataManagement.PackageManagment;
+using ZainCustomerService.DataManagement.UserManagment;
+
+namespace ZainCustomerService.DataManagement
+{
+    /// <summary>
+    /// This class holds information for a single Customer who has a Zain SIM Card.
+    /// </summary>
+    public class User
+    {
+        public string PhoneNumber { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public double Balance { get; set; }
+        public string ExpiryDate { get; set; }
+        public Package UserPackage { get; private set; }
+
+        //constructor ^_^
+        public User(string phone, string firstName, string lastName, double balance, string expiryDate, Package package)
+        {
+            UserManager.CheckIfUserActive(ref expiryDate, ref balance, package.MonthlyPrice);
+            PhoneNumber = phone;
+            FirstName = firstName;
+            LastName = lastName;
+            Balance = balance;
+            ExpiryDate = expiryDate;
+            UserPackage = package;
+        }
+    }
+}
+
+
