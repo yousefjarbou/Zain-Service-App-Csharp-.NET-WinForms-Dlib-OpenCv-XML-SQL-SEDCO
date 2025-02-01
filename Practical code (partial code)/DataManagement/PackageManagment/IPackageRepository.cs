c3aac0d69592e8fb5ce88c186a644a9e019363ee
Yousef Jarbou <166923297+yousefjarbou@users.noreply.github.com>
2025-02-01 15:11:10 +0300

    Add files via upload

---
+++ Practical code (partial code)/DataManagement/PackageManagment/IPackageRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using ZainCustomerService.DataManagement.PackageManagment;
+
+namespace ZainCustomerService.DataManagement.PackageManagment
+{
+    public interface IPackageRepository
+    {
+        public Package GetPackageByName(string name);
+        public List<string> GetPackageNames(bool isPrepaid);
+
+        public Dictionary<string, Package> LoadAllPackages();
+    }
+}
---
+++ Practical code (partial code)/DataManagement/PackageManagment/PackageManager.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Xml.Linq;
+using ZainCustomerService.DataManagement.UserManagment;
+using static ZainCustomerService.Constants.FilePaths;
+using static ZainCustomerService.Constants.XmlElementNames;
+
+namespace ZainCustomerService.DataManagement.PackageManagment
+{
+    public class PackageManager 
+    {
+        private Dictionary<string, Package> _packages;
+        private readonly IPackageRepository _packageRepository;
+
+        public PackageManager(IPackageRepository packageManager)
+        {
+            _packageRepository = packageManager;
+            _packages = _packageRepository.LoadAllPackages();
+        }
+
+        public Package GetPackageByName(string name)
+        {
+            return _packageRepository.GetPackageByName(name);
+        }
+
+        public List<string> GetPackageNames(bool isPrepaid)
+        {
+            return _packageRepository.GetPackageNames(isPrepaid);
+        }
+
+    }
+}
