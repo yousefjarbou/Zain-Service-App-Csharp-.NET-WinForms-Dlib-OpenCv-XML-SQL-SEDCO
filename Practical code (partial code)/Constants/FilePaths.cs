3e5900d86d5c2b1b95c92c57ea1a5cc2db4481d4
Yousef Jarbou <166923297+yousefjarbou@users.noreply.github.com>
2025-02-01 15:12:33 +0300

    Add files via upload

---
+++ Practical code (partial code)/Constants/FilePaths.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.IO;
+
+namespace ZainCustomerService.Constants
+{
+    public static class FilePaths
+    {
+        private static readonly string BaseDirectory = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "XML local Data Files");
+
+        public static readonly string UsersFilePath = Path.Combine(BaseDirectory, "zainUsers.xml");
+        public static readonly string PackagesFilePath = Path.Combine(BaseDirectory, "packages.xml");
+
+        static FilePaths()
+        {
+            // Ensure that the directory exists
+            Directory.CreateDirectory(BaseDirectory);
+        }
+    }
+}
